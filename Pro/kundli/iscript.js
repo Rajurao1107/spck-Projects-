8c4f56e9cf805cde724a02db5b550f18e78a99d8
raju <rajurao1106@gmail.com>
2024-03-05 18:25:46 +0530

    update

---
+++ Pro/kundli/iscript.js
@@ -0,0 +1,333 @@
+function touchmenow(){
+  var a,c;
+  a = document.getElementById("opt1").value;
+  b = document.getElementById("opt2").value;
+  if(a == "1" && b == "sun"){
+    window.open("sun1.html","_self"); 
+  }
+  else if(a == "2" && b == "sun"){
+    window.open("sun2.html","_self");
+  }
+  else if(a == "3" && b == "sun"){
+    window.open("sun3.html","_self");
+  }
+  else if(a == "4" && b == "sun"){
+    window.open("sun4.html","_self");
+  }
+  else if(a == "5" && b == "sun"){
+    window.open("sun5.html","_self");
+  }
+  else if(a == "6" && b == "sun"){
+    window.open("sun6.html","_self");
+  }
+  else if(a == "7" && b == "sun"){
+    window.open("sun7.html","_self");
+  }
+  else if(a == "8" && b == "sun"){
+    window.open("sun8.html","_self");
+  }
+  else if(a == "9" && b == "sun"){
+    window.open("sun9.html","_self");
+  }
+  else if(a == "10" && b == "sun"){
+    window.open("sun10.html","_self");
+  }
+  else if(a == "11" && b == "sun"){
+    window.open("sun11.html","_self");
+  }
+  else if(a == "12" && b == "sun"){
+    window.open("sun12.html","_self");
+  }
+  else if(a == "1" && b == "moon"){
+    window.open("moon1.html","_self");
+  }
+  else if(a == "2" && b == "moon"){
+    window.open("moon2.html","_self");
+  }
+  else if(a == "3" && b == "moon"){
+    window.open("moon3.html","_self");
+  }
+  else if(a == "4" && b == "moon"){
+    window.open("moon4.html","_self");
+  }
+  else if(a == "5" && b == "moon"){
+    window.open("moon5.html","_self");
+  }
+  else if(a == "6" && b == "moon"){
+    window.open("moon6.html","_self");
+  }
+  else if(a == "7" && b == "moon"){
+    window.open("moon7.html","_self");
+  }
+  else if(a == "8" && b == "moon"){
+    window.open("moon8.html","_self");
+  }
+  else if(a == "9" && b == "moon"){
+    window.open("moon9.html","_self");
+  }
+  else if(a == "10" && b == "moon"){
+    window.open("moon10.html","_self");
+  }
+  else if(a == "11" && b == "moon"){
+    window.open("moon11.html","_self");
+  }
+  else if(a == "12" && b == "moon"){
+    window.open("moon12.html","_self");
+  }
+  else if(a == "1" && b == "mercury"){
+    window.open("mercury1.html","_self");
+  }
+  else if(a == "2" && b == "mercury"){
+    window.open("mercury2.html","_self");
+  }
+  else if(a == "3" && b == "mercury"){
+    window.open("mercury3.html","_self");
+  }
+  else if(a == "4" && b == "mercury"){
+    window.open("mercury4.html","_self");
+  }
+  else if(a == "5" && b == "mercury"){
+    window.open("mercury5.html","_self");
+  }
+  else if(a == "6" && b == "mercury"){
+    window.open("mercury6.html","_self");
+  }
+  else if(a == "7" && b == "mercury"){
+    window.open("mercury7.html","_self");
+  }
+  else if(a == "8" && b == "mercury"){
+    window.open("mercury8.html","_self");
+  }
+  else if(a == "9" && b == "mercury"){
+    window.open("mercury9.html","_self");
+  }
+  else if(a == "10" && b == "mercury"){
+    window.open("mercury10.html","_self");
+  }
+  else if(a == "11" && b == "mercury"){
+    window.open("mercury11.html","_self");
+  }
+  else if(a == "12" && b == "mercury"){
+    window.open("mercury12.html","_self");
+  }
+  else if(a == "1" && b == "venus"){
+    window.open("venus1.html","_self");
+  }
+  else if(a == "2" && b == "venus"){
+    window.open("venus2.html","_self");
+  }
+  else if(a == "3" && b == "venus"){
+    window.open("venus3.html","_self");
+  }
+  else if(a == "4" && b == "venus"){
+    window.open("venus4.html","_self");
+  }
+  else if(a == "5" && b == "venus"){
+    window.open("venus5.html","_self");
+  }
+  else if(a == "6" && b == "venus"){
+    window.open("venus6.html","_self");
+  }
+  else if(a == "7" && b == "venus"){
+    window.open("venus7.html","_self");
+  }
+  else if(a == "8" && b == "venus"){
+    window.open("venus8.html","_self");
+  }
+  else if(a == "9" && b == "venus"){
+    window.open("venus9.html","_self");
+  }
+  else if(a == "10" && b == "venus"){
+    window.open("venus10.html","_self");
+  }
+  else if(a == "11" && b == "venus"){
+    window.open("venus11.html","_self");
+  }
+  else if(a == "12" && b == "venus"){
+    window.open("venus12.html","_self");
+  }
+  else if(a == "1" && b == "mars"){
+    window.open("mars1.html","_self");
+  }
+  else if(a == "2" && b == "mars"){
+    window.open("mars2.html","_self");
+  }
+  else if(a == "3" && b == "mars"){
+    window.open("mars3.html","_self");
+  }
+  else if(a == "4" && b == "mars"){
+    window.open("mars4.html","_self");
+  }
+  else if(a == "5" && b == "mars"){
+    window.open("mars5.html","_self");
+  }
+  else if(a == "6" && b == "mars"){
+    window.open("mars6.html","_self");
+  }
+  else if(a == "7" && b == "mars"){
+    window.open("mars7.html","_self");
+  }
+  else if(a == "8" && b == "mars"){
+    window.open("mars8.html","_self");
+  }
+  else if(a == "9" && b == "mars"){
+    window.open("mars9.html","_self");
+  }
+  else if(a == "10" && b == "mars"){
+    window.open("mars10.html","_self");
+  }
+  else if(a == "11" && b == "mars"){
+    window.open("mars11.html","_self");
+  }
+  else if(a == "12" && b == "mars"){
+    window.open("mars12.html","_self");
+  }
+  else if(a == "1" && b == "jupiter"){
+    window.open("jupiter1.html","_self");
+  }
+  else if(a == "2" && b == "jupiter"){
+    window.open("jupiter2.html","_self");
+  }
+  else if(a == "3" && b == "jupiter"){
+    window.open("jupiter3.html","_self");
+  }
+  else if(a == "4" && b == "jupiter"){
+    window.open("jupiter4.html","_self");
+  }
+  else if(a == "5" && b == "jupiter"){
+    window.open("jupiter5.html","_self");
+  }
+  else if(a == "6" && b == "jupiter"){
+    window.open("jupiter6.html","_self");
+  }
+  else if(a == "7" && b == "jupiter"){
+    window.open("jupiter7.html","_self");
+  }
+  else if(a == "8" && b == "jupiter"){
+    window.open("jupiter8.html","_self");
+  }
+  else if(a == "9" && b == "jupiter"){
+    window.open("jupiter9.html","_self");
+  }
+  else if(a == "10" && b == "jupiter"){
+    window.open("jupiter10.html","_self");
+  }
+  else if(a == "11" && b == "jupiter"){
+    window.open("jupiter11.html","_self");
+  }
+  else if(a == "12" && b == "jupiter"){
+    window.open("jupiter12.html","_self");
+  }
+  else if(a == "1" && b == "saturn"){
+    window.open("saturn1.html","_self");
+  }
+  else if(a == "2" && b == "saturn"){
+    window.open("saturn2.html","_self");
+  }
+  else if(a == "3" && b == "saturn"){
+    window.open("saturn3.html","_self");
+  }
+  else if(a == "4" && b == "saturn"){
+    window.open("saturn4.html","_self");
+  }
+  else if(a == "5" && b == "saturn"){
+    window.open("saturn5.html","_self");
+  }
+  else if(a == "6" && b == "saturn"){
+    window.open("saturn6.html","_self");
+  }
+  else if(a == "7" && b == "saturn"){
+    window.open("saturn7.html","_self");
+  }
+  else if(a == "8" && b == "saturn"){
+    window.open("saturn8.html","_self");
+  }
+  else if(a == "9" && b == "saturn"){
+    window.open("saturn9.html","_self");
+  }
+  else if(a == "10" && b == "saturn"){
+    window.open("saturn10.html","_self");
+  }
+  else if(a == "11" && b == "saturn"){
+    window.open("saturn11.html","_self");
+  }
+  else if(a == "12" && b == "saturn"){
+    window.open("saturn12.html","_self");
+  }
+  else if(a == "1" && b == "rahu"){
+    window.open("rahu1.html","_self");
+  }
+  else if(a == "2" && b == "rahu"){
+    window.open("rahu2.html","_self");
+  }
+  else if(a == "3" && b == "rahu"){
+    window.open("rahu3.html","_self");
+  }
+  else if(a == "4" && b == "rahu"){
+    window.open("rahu4.html","_self");
+  }
+  else if(a == "5" && b == "rahu"){
+    window.open("rahu5.html","_self");
+  }
+  else if(a == "6" && b == "rahu"){
+    window.open("rahu6.html","_self");
+  }
+  else if(a == "7" && b == "rahu"){
+    window.open("rahu7.html","_self");
+  }
+  else if(a == "8" && b == "rahu"){
+    window.open("rahu8.html","_self");
+  }
+  else if(a == "9" && b == "rahu"){
+    window.open("rahu9.html","_self");
+  }
+  else if(a == "10" && b == "rahu"){
+    window.open("rahu10.html","_self");
+  }
+  else if(a == "11" && b == "rahu"){
+    window.open("rahu11.html","_self");
+  }
+  else if(a == "12" && b == "rahu"){
+    window.open("rahu12.html","_self");
+  }
+  else if(a == "1" && b == "ketu"){
+    window.open("ketu1.html","_self");
+  }
+  else if(a == "2" && b == "ketu"){
+    window.open("ketu2.html","_self");
+  }
+  else if(a == "3" && b == "ketu"){
+    window.open("ketu3.html","_self");
+  }
+  else if(a == "4" && b == "ketu"){
+    window.open("ketu4.html","_self");
+  }
+  else if(a == "5" && b == "ketu"){
+    window.open("ketu5.html","_self");
+  }
+  else if(a == "6" && b == "ketu"){
+    window.open("ketu6.html","_self");
+  }
+  else if(a == "7" && b == "ketu"){
+    window.open("ketu7.html","_self");
+  }
+  else if(a == "8" && b == "ketu"){
+    window.open("ketu8.html","_self");
+  }
+  else if(a == "9" && b == "ketu"){
+    window.open("ketu9.html","_self");
+  }
+  else if(a == "10" && b == "ketu"){
+    window.open("ketu10.html","_self");
+  }
+  else if(a == "11" && b == "ketu"){
+    window.open("ketu11.html","_self");
+  }
+  else if(a == "12" && b == "ketu"){
+    window.open("ketu12.html","_self");
+  }
+  
+  else{
+    alert("Choose Correct!");
+  }
+}
